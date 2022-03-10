32e9776f0ddca68108331a2f5c873d907c9ef340
NEWSIX <nitigon.phad@gmail.com>
2022-03-10 23:43:54 +0700

    version 2.0.1 (New!)

--- routes/course.js
+++ routes/course.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('course', {});
+  res.render('course', 
+    {
+        question:`<h1 id="scrollspyHeading1">opts</h1>`
+    });
 });
+
+
+// if get จาก course มาเปลี่ยนเทียบ value ที่ส่งมาแล้ว ตรวจสอบว่าจะส่งไปหน้า ejs ไหน 
 
 module.exports = router;
