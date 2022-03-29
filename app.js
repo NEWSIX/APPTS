6a4cc522b2d94f9f5501b557a815d34e4eedb861
Newsix <nitigon.phad@gmail.com>
2022-03-29 10:50:21 +0700

    test db

--- app.js
+++ app.js
@@ -130,21 +130,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("mydb");
-  var myobj = { name: "Company Inc", address: "Highway 37" };
-  dbo.collection("customers").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
-  });
-});
-
-
 module.exports = app;
 
 const port = process.env.PORT || 8080
