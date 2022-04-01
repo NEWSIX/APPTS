a9e3124902994f70dcd0f10148ef066ec769a4ad
NEWSIX <nitigon.phad@gmail.com>
2022-04-01 13:25:31 +0700

    version 2.1.1.2

--- routes/teacher-class.js
+++ routes/teacher-class.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('teacher-class', {});
+  res.render('teacher/teacher-class', {});
 });
 
 module.exports = router;
