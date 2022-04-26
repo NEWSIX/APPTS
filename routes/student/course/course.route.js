f32893528e3acc5bfd63eab7bd778cb93e8060bd
NEWSIX <nitigon.phad@gmail.com>
2022-04-26 11:07:04 +0700

    project

--- routes/student/course/course.route.js
+++ routes/student/course/course.route.js
@@ -1,10 +1,31 @@
 const router = require('express').Router();
+var compiler = require('compilex');
+var options = {stats : true}; //prints stats on console 
+compiler.init(options);
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://appts:Appts123456789@apptsystem.jgb2f.mongodb.net/test";
+const mydatabase = "APPTSystem";
 
 router.get('/', async (req, res, next) => {
   const person = req.user;
-  if(person != undefined){
-
-      res.render('student/course/course_main', { person });
+    if(person != undefined){
+      // PRETEST Check
+      MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(mydatabase);
+        var query = { email: person.email };
+        dbo.collection("StudentAnswer").find(query).toArray(function(err, StudentAnswer) {
+          if (err) throw err;
+          if(Object.keys(StudentAnswer).length === 0){
+            res.redirect('/')
+          }
+          else{
+            res.render('student/course/course_main', { person });
+          }
+          db.close();
+        });
+      });
+      // PRETEST Check
   }
 });
 
